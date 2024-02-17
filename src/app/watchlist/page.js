08cf952e54692cc6b0a7eb960e276f80daf6850a
Pegasus <nwillson2601@gmail.com>
2024-02-17 14:32:41 +0200

    fiexed fetch error

--- src/app/watchlist/page.js
+++ src/app/watchlist/page.js
@@ -1,9 +1,16 @@
 "use client";
-import WatchListCard from "@/components/WatchListCard";
-import { useWatchlist } from "../../context/WatchListContext";
 
+import WatchListCard from "@/components/WatchListCard";
+import { useState, useEffect } from "react";
 function WatchlistPage() {
-  const watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+  const [watchlist, setWatchlist] = useState([]);
+
+  useEffect(() => {
+    // Retrieve watchlist data from localStorage
+    const storedWatchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
+    setWatchlist(storedWatchlist);
+  }, []);
+
   console.log(watchlist);
   return (
     <div className="sm:p-16 py-16 px-8 flex flex-col gap-10 bg-zinc-950 justify-center items-center">
