f55252df484f99cdd5392664f4197e5a68fe0c04
noahwillson <nwillson2601@gmail.com>
2024-03-20 06:26:11 +0200

    Fix: fixed clerk middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,11 @@
 import { authMiddleware } from "@clerk/nextjs";
- 
 // See https://clerk.com/docs/references/nextjs/auth-middleware
 // for more information about configuring your Middleware
  
 export default authMiddleware({
+    apiKey: process.env.CLERK_SECRET_KEY,
+    frontendApiKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    // You can define custom paths for the public and private routes
   // Allow signed out users to access the specified routes:
   publicRoutes: ['/', '/sign-in', '/manga'],
   // Prevent the specified routes from accessing
