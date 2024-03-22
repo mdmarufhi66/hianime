9a0277067939ce1f18d83ee1dd41a14bad35b4cb
noahwillson <nwillson2601@gmail.com>
2024-03-22 05:31:56 +0200

    Fix: edited metadata for sign in page

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,9 +1,14 @@
 import { SignIn } from "@clerk/nextjs";
- 
+
+export const metadata = {
+  title: "ZenAnime | Sign in",
+  description: "Sign in",
+};
+
 export default function Page() {
   return (
-    <div className="flex items-center justify-center">
-        <SignIn />
+    <div className="flex items-center justify-center my-10">
+      <SignIn />
     </div>
   );
 }
