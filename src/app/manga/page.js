d33157ab17a1c2e1f50b8a6ab78a56824b4a6bb3
noahwillson <nwillson2601@gmail.com>
2024-03-22 07:13:51 +0200

    Fix:edited bg color

--- src/app/manga/page.js
+++ src/app/manga/page.js
@@ -9,7 +9,7 @@ export const metadata = {
 
 const Manga = () => {
   return (
-    <div className="sm:p-16 py-16 px-8 flex flex-col gap-10 bg-gradient-to-b from-[#25221e] to-[#8c1105] justify-center items-center">
+    <div className="sm:p-16 py-16 px-8 flex flex-col gap-10 bg-gradient-to-r from-[#25221e] to-[#340750] justify-center items-center">
       <h1 className="justify-self-center text-3xl font-bold">
         Explore <span className="red-gradient">Manga</span>
       </h1>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 function Hero() {
   return (
-    <header className=" sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
+    <header className="container sm:p-16 py-16 px-8 flex justify-center lg:items-center max-lg:flex-col w-full sm:gap-16 gap-0">
       <div className="flex-1 flex flex-col gap-10">
         <h1 className="sm:text-6xl text-5xl text-white lg:max-w-lg font-bold leading-[120%]">
           Explore The <span className="red-gradient">Diverse Realms</span> of
